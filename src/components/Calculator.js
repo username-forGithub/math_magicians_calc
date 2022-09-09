80c4ae59a9edacef7bc28e03e8c7994dc229147c
usernameBakhtiyor <harhil46@gmail.com>
2022-09-09 22:34:19 +0500

    Update files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,64 @@
-import React from 'react';
+/* eslint-disable */
+import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
-export default class Calculator extends React.PureComponent {
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     obj: {
+        total: null,
+        next: null,
+        operation: null,
+      }
+    };
+  }
+
+handleClick = (buttonName)=>this.setState(({obj})=>({obj:calculate(obj,buttonName)}));
+
   render() {
     return (
-      <div className="wrapper">
-        <div className="resultsDiv">0</div>
-        <div className="btnDiv">
-          <input type="button" value="AC" />
-          <input type="button" value="+/-" />
-          <input type="button" value="%" />
-          <input className="orange" type="button" value="&#247;" />
-          <input type="button" value="7" />
-          <input type="button" value="8" />
-          <input type="button" value="9" />
-          <input className="orange" type="button" value="&#215;" />
-          <input type="button" value="4" />
-          <input type="button" value="5" />
-          <input type="button" value="6" />
-          <input className="orange" type="button" value="-" />
-          <input type="button" value="1" />
-          <input type="button" value="2" />
-          <input type="button" value="3" />
-          <input className="orange" type="button" value="+" />
-          <input className="span" type="button" value="0" />
-          <input type="button" value="." />
-          <input className="orange" type="button" value="=" />
+      <div className="calculator-main-cont">
+        
+        <div className="calculator-output">{this.state.obj.next || this.state.obj.total || 0}</div>
+        <div className="calculator-wrapper">
+          <div className="calculator-numbers-cont">
+            <div className="calculator-numbers">
+              <button onClick={()=>this.handleClick("AC")}>AC</button>
+              <button onClick={()=>this.handleClick("+/-")}>+/-</button>
+              <button onClick={()=>this.handleClick('%')}>%</button>
+            </div>
+            <div className="calculator-numbers">
+              <button onClick={()=>this.handleClick('7')}>7</button>
+              <button onClick={()=>this.handleClick('8')}>8</button>
+              <button onClick={()=>this.handleClick('9')}>9</button>
+            </div>
+            <div className="calculator-numbers">
+              <button onClick={()=>this.handleClick('4')}>4</button>
+              <button onClick={()=>this.handleClick('5')}>5</button>
+              <button onClick={()=>this.handleClick('6')}>6</button>
+            </div>
+            <div className="calculator-numbers">
+              <button  onClick={()=>this.handleClick("1")}>1</button>
+              <button  onClick={()=>this.handleClick("2")}>2</button>
+              <button onClick={()=>this.handleClick('3')}>3</button>
+            </div>
+            <div className="calculator-numbers">
+              <button onClick={()=>this.handleClick('0')}>0</button>
+              <button onClick={()=>this.handleClick('.')}>.</button>
+            </div>
+          </div>
+          <div className="calculator-symbol-cont">
+            <button onClick={()=>this.handleClick('÷')}>÷</button>
+            <button onClick={()=>this.handleClick('x')}>×</button>
+            <button onClick={()=>this.handleClick('-')}>-</button>
+            <button  onClick={()=>this.handleClick("+")}>+</button>
+            <button  onClick={()=>this.handleClick("=")}>=</button>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+export default Calculator;
